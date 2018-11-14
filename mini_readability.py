6f88fb3da91f1c619c2254e500b0ae4cf8050ff9
Dmitry Conway <dimaconway@gmail.com>
2018-11-14 19:25:49 +0300

    init commit

---
+++ mini_readability.py
@@ -0,0 +1,82 @@
+# from readability import Document
+import re
+
+from bs4 import BeautifulSoup, Comment
+from urllib import request
+
+# url = 'https://lenta.ru/news/2018/11/14/sankcii/'
+# request.urlretrieve(url, 'lenta.txt')
+
+whitelist = [
+    'blockquote',
+    'em',
+    'i',
+    'img',
+    'strong',
+    'u',
+    'a',
+    'b',
+    'p',
+    'br',
+    'code',
+    'pre'
+]
+
+blacklisted_tags = [
+    'script',
+    'noscript',
+    'style',
+    'iframe',
+    'svg',
+    'nav',
+    'aside'
+]
+
+blacklisted_classes = [
+    'sidebar',
+    'footer',
+    'tabloid',
+    'addition'
+]
+
+text_headers = [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+]
+
+with open('resources/lenta.txt', 'r') as file:
+    webpage = file.read()
+    # doc = Document(webpage)
+    # print(doc.summary())
+    soup = BeautifulSoup(webpage, 'html.parser')
+    body = soup.find('body')
+
+    for tag in body.find_all():
+        is_empty_tag = not tag.get_text(strip=True)
+        is_blacklisted_tag = tag.name.lower() in blacklisted_tags
+        if_blacklisted_class = any(map(
+            lambda class_mask: any(class_mask in attr_class for attr_class in tag.attrs.get('class', [])),
+            blacklisted_classes
+        ))
+        is_header = any('header' in attr_class for attr_class in tag.attrs.get('class', []))
+
+        if is_empty_tag \
+                or is_blacklisted_tag \
+                or if_blacklisted_class:
+            tag.extract()
+        elif is_header:
+            headers = list(map(lambda header: str(header), list(tag.find_all(text_headers))))
+            tag.replace_with(BeautifulSoup(''.join(headers), "html.parser"))
+        # elif tag.contents
+
+    # Extract comments
+    comments = body.findAll(text=lambda text: isinstance(text, Comment))
+    for comment in comments:
+        comment.extract()
+
+    # print(body.prettify())
+    print(body.get_text())
---
+++ test.py
@@ -0,0 +1,72 @@
+# from readability import Document
+import re
+
+from bs4 import BeautifulSoup, Comment
+from urllib import request
+
+# url = 'https://lenta.ru/news/2018/11/14/sankcii/'
+# request.urlretrieve(url, 'lenta.txt')
+
+whitelist = [
+    'blockquote',
+    'em',
+    'i',
+    'img',
+    'strong',
+    'u',
+    'a',
+    'b',
+    'p',
+    'br',
+    'code',
+    'pre'
+]
+
+blacklist = [
+    'script',
+    'noscript',
+    'style',
+    'iframe',
+    'svg',
+]
+
+text_headers = [
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'h5',
+    'h6',
+]
+
+html = """<div><div><div>foo</div></div></div><div>ofo</div><div><h3>bar</h3><h2></h2><h4 class='title'></h4><p>baz</p></div>"""
+soup = BeautifulSoup(html, 'html.parser')
+for tag in soup.find_all():
+    tag.
+    print('{} - {} - {}'.format(tag, tag.children, len(tag.contents)))
+    # print('{} - {}'.format(tag, type(tag)))
+
+
+
+    # print(dir(tag.find_all(text_headers)))
+    # headers = list(map(lambda header: str(header), list(tag.find_all(text_headers))))
+    # if len(headers) > 0:
+    #     tag.replace_with(BeautifulSoup(''.join(headers), "html.parser"))
+
+# with open('lenta.txt', 'r') as file:
+#     webpage = file.read()
+#     # doc = Document(webpage)
+#     # print(doc.summary())
+#     soup = BeautifulSoup(webpage)
+#     body = soup.find('body')
+#
+#     for tag in body.findAll():
+#         if tag.name.lower() in blacklist or not tag.contents:
+#             tag.extract()
+#
+#     # Extract comments
+#     comments = body.findAll(text=lambda text: isinstance(text, Comment))
+#     for comment in comments:
+#         comment.extract()
+#
+print(soup.prettify())
